c9dbd5d892ae0a7c67b5d0fd65b177c9a73470d3
tuankiet1502 <kiet.doan@executionlab.asia>
2022-05-24 11:20:56 +0700

    updated

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem 'i18n-js'
 gem 'tzinfo-data'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
-
+gem 'wdm', '>= 0.1.0'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,5 +20,21 @@ From novices to experts, chatbots are an area in which everyone
 can participate. Exercise your creativity by creating chatbot
 skills for communicating, information, and fun.
 </p>},
+body:
+%{<p>
+<em>Responsive, Intuitive Interfaces with Ruby</em>
+The next step in the evolution of user interfaces is here.
+Chatbots let your users interact with your service in their
+own natural language. Use free and open source tools along
+with Ruby to build creative, useful, and unexpected interactions
+for users. Take advantage of the Lita framework’s step-by-step
+implementation strategy to simplify bot development and testing.
+From novices to experts, chatbots are an area in which everyone
+can participate. Exercise your creativity by creating chatbot
+skills for communicating, information, and fun.
+</p>},
 image_url: 'product.png',
 price: 20.00)
+
+User.delete_all
+User.create!(name:"tuankiet", password:"123456", password_confirmation: "123456")
